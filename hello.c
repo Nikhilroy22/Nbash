f49bc39641d71b40068cb4da1faa22f744c590b3
Nikhilroy22 <nikhilroyfff@gmail.com>
2025-07-11 12:28:18 +0600

    first commit

--- hello.c
+++
@@ -1,7 +0,0 @@
-// hello.c
-#include <stdio.h>
-
-int main() {
-    printf("Hello from Android ARMv7!\n");
-    return 0;
-}
---
+++ src/hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("Hello from Android CLI!\n");
+    return 0;
+}
